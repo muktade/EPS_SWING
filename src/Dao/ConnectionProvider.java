17d7662ea634a141d7245bca741edeeaa126c8d7
muktadeer <muktadeer@gmail.com>
2022-09-12 23:02:34 +0600

    EPS ok

---
+++ src/Dao/ConnectionProvider.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author M. A. MUKTADEER
+ */
+public class ConnectionProvider {
+
+    Connection conn=null;
+    public static Connection getConnectDatabase(){
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn =DriverManager.getConnection("jdbc:mysql://localhost:3306/youtube","root","root123");
+//            JOptionPane.showMessageDialog(null, "Connection to database is successful");
+      
+            return conn;
+           
+            
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+        
+    }
+//    public static void main(String[] args) {
+//        getConnectDatabase();
+//    }
+    
+}
